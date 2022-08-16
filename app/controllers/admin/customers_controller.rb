94b4378d18525039526c8c9a191a608de312138d
EC2 Default User <ec2-user@ip-172-31-18-61.ap-northeast-1.compute.internal>
2022-08-16 14:10:18 +0000

    AddCustomerEdit

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,9 +8,13 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer=Customer.find(params[:id])
   end
 
   def update
+    @customer=Customer.find(params[:id])
+    @customer.update
+    redirect_to '/admin/customers'
   end
   
   private

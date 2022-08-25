4b9c2178884997a4a4f14fd8595b994f39fef2d2
EC2 Default User <ec2-user@ip-172-31-18-61.ap-northeast-1.compute.internal>
2022-08-25 21:43:54 +0000

    退会更新機能

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,6 +1,6 @@
 class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!, only: [:show, :edit, :update]
-  
+
   def show
     @customer = current_customer
   end
@@ -20,15 +20,21 @@ class Public::CustomersController < ApplicationController
   end
 
   def unsubscribe
+     @customer = current_customer
   end
 
   def withdraw
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:alert] = "退会処理が完了しました。"
+    redirect_to top_path
   end
-  
+
   private
 
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
   end
-  
+
 end
